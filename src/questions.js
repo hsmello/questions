5ec59bb48bbf8087a2ceee1dff1d7bb07c6515f9
Hugo <hugosm6@hotmail.com>
2020-03-31 23:54:20 +0100

    10 questions

--- src/questions.js
+++ src/questions.js
@@ -1,28 +1,28 @@
 export default
 [
     {
-        "question": "Qual a cor do ceu?",
+        "question": "Qual das opções ngm gosta?",
         "answers": [
-          {"text": "azul", "correct": true},
-          {"text": "amarelo", "correct": false},
-          {"text": "vermelho", "correct": false},
-          {"text": "verde", "correct": false} ]
+          {"text": "azeite", "correct": false},
+          {"text": "palmito", "correct": true},
+          {"text": "fígado de boi", "correct": false},
+          {"text": "vinho branco", "correct": false} ]
     },
     {
-        "question": "Em que ano estamos?",
+        "question": "Um japonês pode casar com a irmã da sua viúva?",
         "answers": [
-          {"text": "2020", "correct": true},
-          {"text": "1990", "correct": false},
-          {"text": "1800", "correct": false},
-          {"text": "2000", "correct": false} ]
+          {"text": "não", "correct": true},
+          {"text": "sim", "correct": false},
+          {"text": "depende", "correct": false},
+          {"text": "japonês?", "correct": false} ]
     },
     {
-        "question": "Qual o primeiro mês do ano?",
+        "question": "Qual o número máx de cunhados(as) o Hugo pode ter?",
         "answers": [
-          {"text": "abril", "correct": false},
-          {"text": "dezembro", "correct": false},
-          {"text": "janeiro", "correct": true},
-          {"text": "agosto", "correct": false} ]
+          {"text": "4", "correct": false},
+          {"text": "5", "correct": false},
+          {"text": "6", "correct": true},
+          {"text": "7", "correct": false} ]
   },
   {
       "question": "Quantos neuronios o Fanu tem?",
@@ -39,5 +39,45 @@ export default
     {"text": "segunda,", "correct": false},
     {"text": "terceira", "correct": false},
     {"text": "ou quarta?", "correct": false} ]
-}
+},
+{
+  "question": "O Rio de Janeiro...",
+  "answers": [
+    {"text": "continua lindo", "correct": false},
+    {"text": "é a cidade maravilhosa", "correct": false},
+    {"text": "é a pior cidade no planeta", "correct": false},
+    {"text": "é bom pras férias", "correct": true} ]
+},
+{
+  "question": "Quantos meses têm 28 dias?",
+  "answers": [
+    {"text": "1", "correct": false},
+    {"text": "6", "correct": false},
+    {"text": "10", "correct": false},
+    {"text": "12", "correct": true} ]
+},
+{
+  "question": "Amanhã vai dar sol?",
+  "answers": [
+    {"text": "certeza", "correct": false},
+    {"text": "sim", "correct": false},
+    {"text": "não", "correct": false},
+    {"text": "está dando sol agora", "correct": true} ]
+},
+{
+  "question": "Pq ainda estou fazendo perguntas?",
+  "answers": [
+    {"text": "Quero saber sua resposta", "correct": false},
+    {"text": "Não sei as respostas", "correct": false},
+    {"text": "Quero chegar em 10", "correct": true},
+    {"text": "Estou de quarentena", "correct": false} ]
+},
+{
+  "question": "Você se acha capaz de acertar todas?",
+  "answers": [
+    {"text": "claro", "correct": false},
+    {"text": "sim", "correct": false},
+    {"text": "não sei", "correct": false},
+    {"text": "não, jamais", "correct": false} ]
+}  
 ]
